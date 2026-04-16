a7fa850edfa589284b988534695d55ed85f93589
DominoWWW <Cilas@DominoWWW.com>
2026-04-16 13:45:10 +0200

    Implement pre_processer class for handling indentation and whitespace,

---
+++ src/main/pre_processer/pre_processer.py
@@ -0,0 +1,31 @@
+class pre_processer:
+    def __init__(self, data):
+        self.data = data
+
+    def run(self):
+        self.whitespace_strip()
+        self.insert_symbols_at_nested_indents()
+
+    def whitespace_strip(self):
+        self.data = self.data.strip()
+
+    def insert_symbols_at_nested_indents(self):
+        # inserts {} around nested blocks of indented code
+        indent_level = 0
+        indent_level_stack = [0] 
+        data = self.data.splitlines()
+        for i, line in enumerate(data):
+            indent_level = len(line) - len(line.lstrip())
+            if (line.strip() == ""): continue
+            if (indent_level == indent_level_stack[-1]): continue
+
+            if indent_level > indent_level_stack[-1]:
+                data[i-1] += " {"
+                indent_level_stack.append(indent_level)
+            else:
+                while indent_level < indent_level_stack[-1]:
+                    data[i-1] += "}"
+                    indent_level_stack.pop()
+
+        self.data = "\n".join(data)
+
--- src/test/test.py
+++ src/test/test.py
@@ -1,5 +1,22 @@
+from main.pre_processer.pre_processer import pre_processer
+
+
 def test():
-    print("Testing! beep boop!")
+    print("Running tests...")
+    s = """
+        say 1
+        if (a):
+            say 2
+            if (b):
+                say 3
+            say 4
+            if (c):
+                say 5
+        say 6
+    """
+    pre_processer_test = pre_processer(s)
+    pre_processer_test.run()
+    print(pre_processer_test.data)
 
 if __name__ == "__test__":
     test()
