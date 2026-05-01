4819cb01a2a17e0179780816e7b7c44353c8a556
DominoWWW <Cilas@DominoWWW.com>
2026-05-01 12:43:06 +0200

    Refactor controller to accept file path and enhance logging for better debugging

--- src/bosh/app/controller.py
+++ src/bosh/app/controller.py
@@ -2,22 +2,27 @@ from bosh.pre_processor.pre_processor import PreProcessor
 from bosh.parser.parser import parseBosh
 from pathlib import Path
 
+from bosh.app.print import *
 
-def controller():
-    with open("src/test/bosh_examples/big_example.bosh", "r") as f:
-        data = f.read()
-    print("Test:\n" + data)
 
-    processed_code = PreProcessor(data).run()
-    print("Processed Code:\n" + processed_code)
+def controller(bosh_file_path):
+    vprint(f"Opening File Path: \"{bosh_file_path}\"...")
+    with open(bosh_file_path, "r") as f:
+        content = f.read()
+    vvprint(indent(content))
 
+    vprint("Pre Processing...")
+    processed_code = PreProcessor(content).run()
+    vvprint(indent(processed_code))
+
+    vprint("Parsing...")
     ast = parseBosh(processed_code)
-    print("AST:\n" + str(ast))
+    vvprint(indent(ast))
 
-    print("Analyzing...")
+    vprint("Analyzing...")
     # Semantisk analyse
-    print("Analyzed.")
+    #print("Analyzed.")
 
-    print("Executing...")
+    vprint("Executing...")
     # Interpreter
-    print("Done.")
+    #print("Done.")
