05db4eb2635b2f8bf9b0270bf0399df5639846f8
DominoWWW <Cilas@DominoWWW.com>
2026-05-01 09:41:29 +0200

    Implement controller function to handle Bosh code processing and simpify main

---
+++ src/bosh/app/controller.py
@@ -0,0 +1,23 @@
+from bosh.pre_processor.pre_processor import PreProcessor
+from bosh.parser.parser import parseBosh
+from pathlib import Path
+
+
+def controller():
+    with open("src/test/bosh_examples/big_example.bosh", "r") as f:
+        data = f.read()
+    print("Test:\n" + data)
+
+    processed_code = PreProcessor(data).run()
+    print("Processed Code:\n" + processed_code)
+
+    ast = parseBosh(processed_code)
+    print("AST:\n" + str(ast))
+
+    print("Analyzing...")
+    # Semantisk analyse
+    print("Analyzed.")
+
+    print("Executing...")
+    # Interpreter
+    print("Done.")
--- src/main.py
+++ src/main.py
@@ -1,26 +1,9 @@
-from bosh.pre_processor.pre_processor import PreProcessor
-from bosh.parser.parser import parseBosh
 from pathlib import Path
 
+from bosh.app.controller import controller
 
 def main():
-    with open("src/test/bosh_examples/big_example.bosh", "r") as f:
-        data = f.read()
-    print("Test:\n" + data)
-
-    processed_code = PreProcessor(data).run()
-    print("Processed Code:\n" + processed_code)
-
-    ast = parseBosh(processed_code)
-    print("AST:\n" + str(ast))
-
-    print("Analyzing...")
-    # Semantisk analyse
-    print("Analyzed.")
-
-    print("Executing...")
-    # Interpreter
-    print("Done.")
+    controller()
 
 
 if __name__ == "__main__":
