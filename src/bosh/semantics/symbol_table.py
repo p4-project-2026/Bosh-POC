644cc0476f71153e7830e944764566f4c4d1dda3
Michael Pedersen <mspe24@student.aau.dk>
2026-05-05 11:21:55 +0200

    Remove ScopeStack class and its methods to streamline symbol table management

--- src/bosh/semantics/symbol_table.py
+++ src/bosh/semantics/symbol_table.py
@@ -89,38 +89,6 @@ class SymbolTable(Generic[T]):
 
     
 
-class ScopeStack(Generic[T]):
-    def __init__(self,  table: Optional[SymbolTable[T]] = SymbolTable[T](persistent=True)):
-        self.table = table
-
-    def new_scope(self, persistent: bool = True):
-        self.table = self.table.new_scope(persistent=persistent)
-
-    def exit_scope(self):
-        try:
-            self.table = self.table.exit_scope()
-        except Exception as e:
-            raise Exception("Cannot exit global scope.")
-        
-    
-    
-
-    def bind(self, name: str, value: T):
-        try:    
-            self.table.bind(name, value)
-        except Exception as e:
-            raise Exception(f"Variable '{name}' already bound to a different type in local scope.")
-
-    def lookup(self, name: str) -> Optional[T]:
-        try:
-            return self.table.lookup(name)
-        except Exception as e:
-            raise Exception(f"Variable '{name}' not found in any scope.")
-        
-    def domain(self) -> List[str]:
-        return self.table.domain()
-
-
 
 
 
