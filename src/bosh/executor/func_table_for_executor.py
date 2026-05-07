d442c58e9ff8dcaf7a2974bb01bc9971dbee1692
Michael Pedersen <mspe24@student.aau.dk>
2026-05-07 12:44:36 +0200

    Implement function management methods in FuncTableForExecutor class

--- src/bosh/executor/func_table_for_executor.py
+++ src/bosh/executor/func_table_for_executor.py
@@ -22,3 +22,23 @@ class FunctionDef:
 def FuncTableForExecutor(SymbolTable[FunctionDef]):
     def __init__(self, parent: Optional['FuncTableForExecutor'] = None, write_through: bool = True):
         super().__init__(parent=parent, write_through=write_through)
+    
+    def new_scope(self):
+        raise Exception("Cannot create new scope for function definitions. Function definitions are global.")
+    
+    def exit_scope(self):
+        raise Exception("Cannot exit scope for function definitions. Function definitions are global.")
+    
+    def update(self, name, type_value):
+        raise Exception("Cannot update function definitions. Function definitions are global and immutable.")
+    
+    def bind(self, name: str, function_def: FunctionDef):
+        if name in self.table:
+            raise Exception(f"Function '{name}' already defined in scope.")
+        self.table[name] = function_def
+    
+    def lookup(self, name: str) -> FunctionDef:
+        if name in self.table:
+            return self.table[name]
+        raise Exception(f"Function '{name}' not found in scope.")
+    
