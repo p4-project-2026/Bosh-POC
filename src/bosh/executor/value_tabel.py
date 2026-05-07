0e070a61d0f6e2719f44751d75d97db26b4c1738
Michael Pedersen <mspe24@student.aau.dk>
2026-05-07 10:31:26 +0200

    Refactor ValueTable: encapsulate cell values in Cell dataclass and enhance scope management methods

--- src/bosh/executor/value_tabel.py
+++ src/bosh/executor/value_tabel.py
@@ -1,52 +1,50 @@
+from dataclasses import dataclass
+
 from bosh.semantics.symbol_table import SymbolTable
 from typing import Dict, Optional, Any, TypeVar
 
-class ValueTable(SymbolTable[Any]):
-    def __init__(self, parent: Optional['ValueTable'] = None, write_through: bool = True):
+@dataclass
+class Cell:
+    value: Any
+
+class ValueTable(SymbolTable[Cell]):
+    def __init__(self, parent: Optional['ValueTable[Cell]'] = None, write_through: bool = True):
         super().__init__(parent=parent, write_through=write_through)
     
     def new_scope(self, write_through: bool = True) -> 'ValueTable':
         return ValueTable(parent=self, write_through=write_through)
 
-    def exit_scope(self) -> 'ValueTable':
+    def exit_scope(self):
         if self.parent is None:
             raise Exception("Cannot exit global scope.")
         return self.parent
     
-    def snapshot(self) -> Dict[str, Any]:
-        scopes = []
-        scope = self
-        while scope is not None:
-            scopes.append(scope)
-            scope = scope.parent
-        
-        
-        snapshot_table: Dict[str, Any] = {}
-
-        for scope in reversed(scopes):
-            snapshot_table.update(scope.table)
-        
-        
-        return snapshot_table
+    def snapshot_table(self) -> 'ValueTable[Cell]':
+        snapshot = ValueTable()
+        snapshot.table = self.snapshot()
+        return snapshot
 
     def bind_local(self, name: str, value: Any):
         #this is for x in "for all X in y"
-        self.table[name] = value
+        if name in self.table:
+            self.table[name] = Cell(value)
+            return # If variable is already bound to the same type, do nothing
+        self.table[name] = Cell(value)
 
     def bind (self, name: str, value: Any):
         if name in self.table:
-            self.table[name] = value
+            self.table[name] = Cell(value)
             return # If variable is already bound to the same type, do nothing
         if self.write_through and self.parent is not None:
             # Check if variable is already defined in a parent scope with the same type
             if self.parent.update(name, value):
                     return
-        self.table[name] = value
+        self.table[name] = Cell(value)
 
             
-    def update(self, name: str, value: Any) -> Optional[bool]:
+    def update(self, name: str, value: Any) -> bool:
         if name in self.table:
-            self.table[name] = value
+            self.table[name] = Cell(value)
             return True # If variable is already bound to the same type, do nothing
         elif self.parent is not None and self.write_through:
             return self.parent.update(name, value)
@@ -55,8 +53,7 @@ class ValueTable(SymbolTable[Any]):
         
     def lookup(self, name: str) -> Any:
         if name in self.table:
-            return self.table[name]
-        # do we use write_through here to hide for functions? 
+            return self.table[name].value
         elif self.parent is not None:
             return self.parent.lookup(name)
         
