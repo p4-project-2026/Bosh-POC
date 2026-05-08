299f8ea68757eb2a2fcefe4d831ca8fcd85f6f47
Michael Pedersen <mspe24@student.aau.dk>
2026-05-08 12:27:52 +0200

    Fix FunctionBinding class: rename parameter from 'the_function_scope' to 'captured_scope' for clarity

--- src/bosh/executor/function_binding.py
+++ src/bosh/executor/function_binding.py
@@ -3,7 +3,7 @@ from bosh.abstract_syntax.ast_base import Block
 
 
 class FunctionBinding:
-    def __init__(self, parameters: list[str], the_function_scope: VarTable, body: Block):
+    def __init__(self, parameters: list[str], captured_scope: VarTable, body: Block):
         self.parameters = parameters
-        self.the_function_scope = the_function_scope
+        self.captured_scope = captured_scope
         self.body = body
