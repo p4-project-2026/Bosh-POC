a3e13b27ef0b6854eb76a9d68bc4d68636411f7c
Michael Pedersen <mspe24@student.aau.dk>
2026-05-08 09:49:57 +0200

    Fix VarTable class: update constructor to use correct default value for function_scope

--- src/bosh/executor/var_table.py
+++ src/bosh/executor/var_table.py
@@ -2,8 +2,8 @@ from bosh.executor.table import Table
 
 
 class VarTable(Table[int]):
-    def __init__(self, function_scope: bool = True):
-        super().__init__(functionscope=function_scope)
+    def __init__(self, function_scope: bool = False):
+        super().__init__(function_scope=function_scope)
     
 
     
