b4eb32982a1e6fd4e5a8effb1654165999e43861
Michael Pedersen <mspe24@student.aau.dk>
2026-05-08 10:08:33 +0200

    Fix VarTable class: consolidate constructor methods and ensure proper initialization of table attribute

--- src/bosh/executor/var_table.py
+++ src/bosh/executor/var_table.py
@@ -5,6 +5,12 @@ class VarTable(Table[int]):
     def __init__(self, function_scope: bool = False):
         super().__init__(function_scope=function_scope)
     
+    def __init__(self, table: dict[str, int]):
+        self.function_scope = False
+        self.table = table.copy()
+    
+    def get_snapshot(self):
+        return self.table.copy()
 
     
  
