14ab6c1f6e5507bd59f16c87b18731ba83b33bb7
Michael Pedersen <mspe24@student.aau.dk>
2026-05-11 11:28:56 +0200

    Refactor FunctionBinding: move Block import inside TYPE_CHECKING for better type hinting

--- src/bosh/executor/function_binding.py
+++ src/bosh/executor/function_binding.py
@@ -1,9 +1,12 @@
 from bosh.executor.var_table import VarTable
-from bosh.abstract_syntax.ast_base import Block
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from bosh.abstract_syntax.ast_base import Block
 
 
 class FunctionBinding:
-    def __init__(self, parameters: list[str], captured_scope: VarTable, body: Block):
+    def __init__(self, parameters: list[str], captured_scope: VarTable, body: "Block"):
         self.parameters = parameters
         self.captured_scope = captured_scope
         self.body = body
